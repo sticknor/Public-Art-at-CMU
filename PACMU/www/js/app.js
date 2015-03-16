5cbc2efe63d42810c837b2162e45798365d151a2
akshalaby <akshalaby@gmail.com>
2015-03-16 16:31:31 -0400

    navigation working to /map

--- PACMU/www/js/app.js
+++ PACMU/www/js/app.js
@@ -18,71 +18,20 @@ angular.module('starter', ['ionic'])
   });
 })
 
-.config(function($stateProvider) {
+.config(function($stateProvider, $urlRouterProvider){
   $stateProvider
-
     .state('app', {
       url: "/",
       templateUrl: "index.html"
     })
-    .state('app.map', {
+    .state('map', {
       url: "/map",
       views: {
-        'menuContent' :{
-          templateUrl: "templates/maps.html",
-          controller: 'mapCtrl'
+        map:{
+          templateUrl: "templates/maps.html"
         }
       }
-    })
+    });
 
-})
-
-
-//MAPPE
-.controller('mapCtrl', function($scope, $ionicLoading, $compile) { 
-      $scope.init = function() {
-        var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
-        var mapOptions = {
-          center: myLatlng,
-          zoom: 16,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map"),
-            mapOptions);
-
-        //Marker + infowindow + angularjs compiled ng-click
-        var contentString = "<div><a ng-click='clickTest()'>Clicca qui!</a></div>";
-        var compiled = $compile(contentString)($scope);
-        var infowindow = new google.maps.InfoWindow({
-          content: compiled[0]
-        });
-        var marker = new google.maps.Marker({
-          position: myLatlng,
-          map: map,
-          title: 'Uluru (Ayers Rock)'
-        });
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.open(map,marker);
-        });
-        $scope.map = map;
-    };
-    // google.maps.event.addDomListener(window, 'load', initialize);
-    $scope.centerOnMe = function() {
-        if(!$scope.map) {return;}
-
-        $scope.loading = $ionicLoading.show({
-          content: 'Getting current location...',
-          showBackdrop: false
-        });
-
-        navigator.geolocation.getCurrentPosition(function(pos) {
-          $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-          $scope.loading.hide();
-        }, function(error) {
-          alert('Unable to get location: ' + error.message);
-        });
-    };
-    $scope.clickTest = function() {
-        alert('Example of infowindow with ng-click')
-    };
+    //$urlRouterProvider.otherwise('/');
 })
